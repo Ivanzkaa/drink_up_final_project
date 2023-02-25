706c81f6d976c35c514eb73ae9b605f095902952
spcruz5 <spcruz5@gmail.com>
2023-02-25 13:52:03 -0500

    Adding all drinks name for each spirit preference in switch case selector file

--- public/src/index.js
+++ public/src/index.js
@@ -1,8 +1,4 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-import { getDatabase, set, get, ref} from "firebase/database";
-import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
-import { AggregateField, getFirestore } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js';
 
 const firebaseApp = initializeApp({
   apiKey: "AIzaSyCkVFHVxgQigRuvRNwByMcXzR_q3vxzSWg",
@@ -14,33 +10,3 @@ const firebaseApp = initializeApp({
   appId: "1:49108513412:web:ada66590542f189d5b88d1",
   measurementId: "G-8MWJ6SWSN7"
 });
-
-// Call methods from firebase
-const auth = getAuth(firebaseApp);
-const db = getFirestore(firebaseApp);
-
-// Initialize Realtime Database and get a reference to the service
-const database = getDatabase(app);
-
-
-//USER DATA 
-
-var spiritBtn = document.querySelector("#submit-button1");
-let preferenceBtn = document.getElementById("submit-button2");
-
-function SpiritData() {
-  set(ref(db, "Spirit/"), {
-    Spirit : spiritBtn.id
-  })
-}
-
-function PreferenceData() {
-  set(ref(db, "Preference/"), {
-    Preference : preferenceBtn.id
-})
-}
-
-
-
-spiritBtn.addEventListener('click', SpiritData);
-preferenceBtn.addEventListener('click', PreferenceData);
